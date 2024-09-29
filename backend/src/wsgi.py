1bc4b04efc3f08c164dd5b412c631e5663d6d088
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2024-09-29 22:46:15 +0100

    fix: import error in production - wsgi

--- backend/src/wsgi.py
+++ backend/src/wsgi.py
@@ -1,5 +1,9 @@
-import config
-from server import server as nifoda
+try:
+    from . import config
+    from .server import server as nifoda
+except ImportError:
+    import config
+    from server import server as nifoda
 
 if __name__ == "__main__":
     if config.environment == "PROD":
