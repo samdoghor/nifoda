3e566ffa4a9cdd1508750e427da76aea93ae65cd
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2024-09-29 23:06:21 +0100

    fix: import error in production - index

--- backend/src/routes/index.py
+++ backend/src/routes/index.py
@@ -37,7 +37,10 @@ DELETE /categories/<category_id>
 
 from flask import Blueprint
 
-from resources import IndexResource
+try:
+    from ..resources import IndexResource
+except ImportError:
+    from resources import IndexResource
 
 # configuration
 
